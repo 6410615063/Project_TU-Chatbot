7afdfeb1b78268b20a3defa1336d7479d3a333e5
6410615063 <6410615063@student.tu.ac.th>
2025-04-08 11:07:09 +0700

    Remove unused functions inside chat.py

--- Chatbot/chat.py
+++ Chatbot/chat.py
@@ -2,62 +2,6 @@ from . import chatbot
 from .models import Profile, Chat
 from django.contrib.auth.models import User
 
-chatlog = []
-# # get chat from the list above
-# def get_chatlog() :
-#     if (len(chatlog) == 0) :
-#         #chatlog.append("Hello, how can I help you?")
-#         chatlog.append({"role": "assistant", "content": "Hello, how can I help you?"})
-#     chatlog_content = [message["content"] for message in chatlog]
-#     return chatlog_content
-
-# # get chat from a specific obj of the Chat model in the DB
-# def get_chatlog_2() :
-#     chat = Chat.objects.get(name='TestChat')
-#     chat_content = [message["content"] for message in chat.messages]
-#     return chat_content
-
-# # get user -> profile -> chat -> message, but fixed user
-# def get_chatlog_3() :
-#     user = User.objects.get(username='TestUser')
-#     profile = user.profile
-#     chats = profile.chats.all()
-#     chat = chats[0]
-#     chat_content = [message["content"] for message in chat.messages]
-#     return chat_content
-
-# # like get_chatlog_3, but from given user & chat name
-# def get_chatlog_4(user_name, chat_name) :
-#     user = User.objects.get(username=user_name)
-#     profile = user.profile
-#     chats = profile.chats.all()
-
-#     # get the user's chat based on given name
-#     filtered_chats = [chat for chat in chats if chat.name == chat_name]
-#     if len(filtered_chats) == 1 :
-#         chat = filtered_chats[0]
-#     else :
-#         chat = chats[0]
-
-#     chat_content = [message["content"] for message in chat.messages]
-#     return chat_content
-
-# def get_chatlog_5(user, chat_name) :
-#     profile = user.profile
-#     chats = profile.chats.all()
-
-#     # get the user's chat based on given name
-#     filtered_chats = [chat for chat in chats if chat.name == chat_name]
-#     if len(filtered_chats) == 1 :
-#         chat = filtered_chats[0]
-#         print('chat found')
-#     else :
-#         chat = chats[0]
-#         print('chat not found')
-
-#     chat_content = [message["content"] for message in chat.messages]
-#     return chat_content
-
 def get_chatlog_6(user, chat_name) :
     profile = user.profile
     chats = profile.chats.all()
@@ -74,14 +18,6 @@ def get_chatlog_6(user, chat_name) :
     # prepare chatlog for displaying
     chat_content = [message["content"] for message in chat.messages]
     return chat_content
-
-# def get_chats(user_name) :
-#     user = User.objects.get(username=user_name)
-#     profile = user.profile
-#     chats = profile.chats.all()
-#     chat_name = [chat.name for chat in chats]
-
-#     return chat_name
 
 # format chatlog for displaying
 def get_chats_2(user) :
@@ -133,19 +69,6 @@ def delete_chat(user, chat_name) :
         return chats[0].name
     else :
         return ''
-
-def append_chatlog(message) :
-    chatlog.append(message)
-
-def user_chat(message) :
-    # add user message to log
-    append_chatlog({"role": "user", "content": message})
-
-    # generate chatbot's message
-    chatbot_message = chatbot.generate_chatbot_message(chatlog, "claude-haiku")
-
-    # add user message to log
-    append_chatlog({"role": "assistant", "content": chatbot_message})
 
 def user_chat(user, chat_name, message_new) :
     profile = user.profile
@@ -211,6 +134,3 @@ def guest_chat(chats, message_new) :
     chatlog_display = [message['content'] for message in chatlog]
 
     return chatlog_display
-
-def reset() :
-    chatlog.clear()
